1d97dafdf3ac367ffe1e78e871825362637b7c82
Kaproncai <kaproncai.tamas@dit.hu>
2022-07-15 20:22:36 +0200

    minimum option for wmake

--- wmake.bat
+++ wmake.bat
@@ -1,4 +1,10 @@
 .\bin\wat2wasm.exe %1.wat -o %1.wasm
+
 certutil -encodehex -f %1.wasm %1.b64 0x40000001
-copy /b head.htm + %1.b64 + tail.htm %1.htm
+
+IF [%2] == [] (
+ copy /b head.htm + %1.b64 + tail.htm %1.htm
+) else (
+ copy /b head_min.htm + %1.b64 + tail_min.htm %1.htm
+)
  
